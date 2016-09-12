d925d6e0ae6644f6aa40f72f7de80e23d702ec6f
tanyinqing <289367447@qq.com>
2016-09-12 16:49:35 +0800

    0912

--- src/com/example/utilspeckage/MainActivity.java
+++ src/com/example/utilspeckage/MainActivity.java
@@ -2,6 +2,14 @@ package com.example.utilspeckage;
 
 
 
+import java.util.List;
+
+import com.example.utilspeckage.entity.District;
+import com.example.utilspeckage.listener.DataListener;
+import com.example.utilspeckage.web.BaseWeb;
+import com.example.utilspeckage.web.OrderWeb;
+import com.lidroid.xutils.http.RequestParams;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.util.Log;
@@ -15,6 +23,21 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-	}
+		OrderWeb mOrderWeb=new OrderWeb(this);
+		mOrderWeb.goToPay(new DataListener<List<District>>(){
+			@Override
+			public void onSuccess(List<District> data) {
+				for(int i=1;i<data.size();i++){
+					Log.d("tanyinqing", data.toString());
+					Log.d("tanyinqing", "*************************************");
+					Log.d("tanyinqing", "*************************************");
+					Log.d("tanyinqing", "*************************************");
+				}
+				
+			}
+		});
+		
+	}	
+	
 
 }
