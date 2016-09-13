7b163ce47082a7007859a7960852e81ffdcea3fb
tanyinqing <289367447@qq.com>
2016-09-13 12:21:05 +0800

    0912

--- src/com/example/utilspeckage/MainActivity.java
+++ src/com/example/utilspeckage/MainActivity.java
@@ -1,19 +1,22 @@
 package com.example.utilspeckage;
-
-
-
 import java.util.List;
 
 import com.example.utilspeckage.entity.District;
 import com.example.utilspeckage.listener.DataListener;
+import com.example.utilspeckage.utils.MyDialog;
+import com.example.utilspeckage.utils.MyToast;
 import com.example.utilspeckage.web.BaseWeb;
 import com.example.utilspeckage.web.OrderWeb;
 import com.lidroid.xutils.http.RequestParams;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Toast;
 
 //123
 public class MainActivity extends Activity {
@@ -22,7 +25,11 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		 //搜索
+                   }
+		
 	}	
 	
 
-}
+
