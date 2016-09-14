7b527bd1dc51ad6b58d4c3e4cdb7dcec66e6d58d
tanyinqing <289367447@qq.com>
2016-09-14 11:07:30 +0800

    0912

--- src/com/example/utilspeckage/MainActivity.java
+++ src/com/example/utilspeckage/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.utilspeckage;
+import java.util.ArrayList;
 import java.util.List;
 
+import com.example.utilspeckage.Constant.Constant;
 import com.example.utilspeckage.entity.District;
 import com.example.utilspeckage.listener.DataListener;
 import com.example.utilspeckage.utils.MyDialog;
 import com.example.utilspeckage.utils.MyToast;
+import com.example.utilspeckage.utils.ShowUtil;
 import com.example.utilspeckage.web.BaseWeb;
 import com.example.utilspeckage.web.OrderWeb;
 import com.lidroid.xutils.http.RequestParams;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.text.InputFilter;
 import android.text.InputType;
 import android.util.Log;
@@ -18,16 +23,18 @@ import android.view.Menu;
 import android.view.View;
 import android.widget.Toast;
 
-//123
+//ShowUtil.showToast(mContext,"支付宝");   测试语句
 public class MainActivity extends Activity {
-
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
-		 //搜索
-                   }
+		
+		
+       }
+	
 		
 	}	
 	
